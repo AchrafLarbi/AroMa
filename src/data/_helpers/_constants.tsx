7ee70425f174cb45a5856d33e123080db94a2cb8
AchrafLarbi <larbiachraf03@gmail.com>
2025-09-18 11:00:00 +0100

    feat: update and improvements

--- src/data/_helpers/_constants.tsx
+++ src/data/_helpers/_constants.tsx
@@ -2,3 +2,5 @@ export const itemsPerPage = 12;
 export const domain = 'https://aromaa.vercel.app';
 export const baseUrl = 'https://dummyjson.com/recipes'
 // // Refactored code - 6005
+
+// // Refactored code - 1099
